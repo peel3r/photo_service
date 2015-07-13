d5cde01dfdfd141bdb7a86df1e79f8f8ba0d1b80
peel3r <p@weljakubowski.com>
2015-07-13 15:22:20 +0100

    initial setup + photo_search model and spec

---
+++ app/models/photo_search.rb
@@ -0,0 +1,33 @@
+class PhotoSearch
+
+  include ActiveModel::Model
+
+  SIZES = [350,800]
+  CAMERAS = %w(f i r 4 5 6 7 8 9 10 11 12)
+  BASE_URL = "http://vcache.arnoldclark.com/imageserver"
+
+  attr_accessor :registration, :stock_ref
+
+  validates :registration, presence: true, length: { minimum: 7 }, allow_blank: false
+  validates :stock_ref, presence: true, length: { minimum: 9 }, allow_blank: false
+
+  def initialize(stock_ref = "", registration = "" )
+    @stock_ref = stock_ref.strip.upcase.gsub(' ', '')
+    @registration = registration.strip.upcase.gsub(' ', '')
+  end
+
+  def fetch_images(reference="")
+    @images = []
+    SIZES.each do |size|
+      CAMERAS.each do |camera|
+        @images << [BASE_URL, reference,size, camera].join('/')
+      end
+    end
+    @images
+  end
+
+
+  def obfuscated_ref
+    @reference = @stock_ref.chars.take(@registration.length).zip(@registration.reverse.chars).flatten.join << @stock_ref.chars[8]
+  end
+end
---
+++ spec/models/photo_searches_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+describe PhotoSearch do
+
+  subject {described_class.new(stock_ref= '' ,registration= '')}
+
+
+  context  'test validations' do
+    it 'for registration ' do
+      expect(subject.valid?).to eq false
+      expect(subject.errors[:registration]).to eq(["can't be blank", "is too short (minimum is 7 characters)"])
+
+    end
+    it 'for stock_ref ' do
+      expect(subject.valid?).to eq false
+      expect(subject.errors[:stock_ref]).to eq(["can't be blank", "is too short (minimum is 9 characters)"])
+
+    end
+
+  end
+  context 'it process the form with valid values' do
+    it 'accepts registration value' do
+      allow(subject).to receive(:registration) {'abc'}
+      expect(subject.registration).to eq('abc')
+
+    end
+    it 'accepts stock_ref value' do
+      allow(subject).to receive(:stock_ref) {'bb2-b'}
+      expect(subject.stock_ref).to eq('bb2-b')
+
+    end
+  end
+
+  describe '.obfuscated_stock_ref' do
+    subject {described_class.new( stock_ref= ' arnfa-u-4633 ',registration= ' mk5 1fdd ')}
+
+    it 'calculates reference' do
+      expect(subject.obfuscated_ref).to eq('ADRDNFF1A5-KUM4')
+    end
+
+  end
+
+
+  describe 'fetch images' do
+    subject {described_class.new(stock_ref= ' arnfa-u-4633 ', registration= ' mk5 1fdd ')}
+    it 'returns correct image set size' do
+      expect(subject.fetch_images.size).to eq 24
+
+    end
+    it 'returns expected urls' do
+      expect(subject.fetch_images('ADRDNFF1A5-KUM4')).to eq [
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/f',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/i',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/r',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/4',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/5',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/6',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/7',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/8',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/9',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/10',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/11',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/350/12',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/f',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/i',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/r',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/4',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/5',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/6',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/7',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/8',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/9',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/10',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/11',
+                                                                    'http://vcache.arnoldclark.com/imageserver/ADRDNFF1A5-KUM4/800/12'
+                                                                ]
+    end
+  end
+end
+
