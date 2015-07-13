19fe467b45844a771be286ca907ca8269a0a70c4
peel3r <p@weljakubowski.com>
2015-07-13 19:33:34 +0100

    added rspec test

--- spec/controllers/photo_searches_controller_spec.rb
+++ spec/controllers/photo_searches_controller_spec.rb
@@ -1,18 +1,21 @@
 require 'rails_helper'
 
 describe PhotoSearchesController do
-  describe 'GET #index' do
-
-
+  describe 'GET #new' do
     context 'without params' do
-
+      it 'renders the :new template' do
+        get :new
+        expect(response).to render_template :new
       end
 
-      it 'renders the :new template' do
+      context 'without params' do
+        let(:ref)  {'ADRDNFF1A5-KUM4'}
 
-
+        it 'renders the :photo_searches#index template' do
+          get :new, ref
+          expect(response).to render_template :new
+        end
+      end
     end
-
-
   end
 end
